f2cfafd7627bdf786b8a11a5fcd7ffdbf57613c6
Lars Vågenes <31071966+h181422@users.noreply.github.com>
2018-11-21 13:45:46 +0100

    Add project files.

---
+++ oppg3/Commons.cs
@@ -0,0 +1,140 @@
+﻿using System;
+/// <summary>
+/// Denne klassen inneholder nyttige metoder som ikke burde være nødvendig å skrive på nytt hver gang de brukes.
+/// </summary>
+public static class Commons
+{
+
+   /// <summary>
+   /// Spør brukeren etter en gyldig int
+   /// </summary>
+   /// <param name="Feilmelding">Feilmelding som skal vises til brukeren</param>
+   /// <param name="maxValue">Høyeste verdi vi tillater</param>
+   /// <param name="minValue">Laveste verdi vi tillater</param>
+   /// <returns>En gyldig int fra bruker</returns>
+    public static int GetIntConsole(string Feilmelding = "", int maxValue = Int32.MaxValue, int minValue = Int32.MinValue)
+    {
+        int i;
+        while (!(int.TryParse(Console.ReadLine(), out i)))
+        {
+            if (Feilmelding.Length > 0)
+                Console.WriteLine(Feilmelding);
+        }
+
+        if(i<minValue || i> maxValue){
+            Console.WriteLine("Out of scope! Skriv inn et tall som er mellom {0} og {1}", minValue, maxValue);
+            return GetIntConsole(Feilmelding, maxValue, minValue);
+        }
+        return i;
+
+    }
+
+    /// <summary>
+    /// Spør brukeren etter en gyldig double
+    /// </summary>
+    /// <param name="Feilmelding">Feilmelding som skal vises til brukeren</param>
+    /// <returns>En gyldig double fra brukeren</returns>
+    public static double GetDoubleConsole(string Feilmelding = "")
+    {
+        double i;
+
+        while (!(double.TryParse(Console.ReadLine(), out i)))
+        {
+            if (Feilmelding.Length > 0)
+                Console.WriteLine(Feilmelding);
+        }
+        return i;
+
+    }
+
+    /// <summary>
+    /// Skriver en tekst til fil. Valgfritt om du skal overskrive eller legge til tekst.
+    /// </summary>
+    /// <param name="filplassering">plasseringen til filen + navnet og filtypen eks: test.txt</param>
+    /// <param name="textTilFil">Teskten som skal skrives til fil</param>
+    /// <param name="append">true=legg til tekst, false=fjern eksisterende fil og skriv ny</param>
+    public static void SkrivTilFil(string filplassering, string textTilFil, bool append = false)
+    {
+        if (append)
+        {
+            using (System.IO.StreamWriter sw = System.IO.File.AppendText(filplassering))
+            {
+                sw.WriteLine(textTilFil);
+            }
+        }
+        else
+        {
+            System.IO.File.WriteAllText(filplassering, textTilFil);
+        }
+
+    }
+
+    /// <summary>
+    /// Skriver en tekst til fil. Valgfritt om du skal overskrive eller legge til tekst.
+    /// </summary>
+    /// <param name="filplassering">plasseringen til filen + navnet og filtypen eks: test.txt</param>
+    /// <param name="textTilFil">Teskten som skal skrives til fil. Skriver hver del av tabellen på ny linje</param>
+    /// <param name="append">true=legg til tekst, false=fjern eksisterende fil og skriv ny</param>
+    public static void SkrivTilFil(string filplassering, string[] textTilFil, bool append = false)
+    {
+        if (append)
+        {
+            using (System.IO.StreamWriter sw = System.IO.File.AppendText(filplassering))
+            {
+                foreach(string line in textTilFil)
+                {
+                    sw.WriteLine(line);
+                }
+            }
+        }
+        else
+        {
+            System.IO.File.WriteAllLines(filplassering, textTilFil);
+        }
+
+    }
+
+    /// <summary>
+    /// Åpner en fil og leser den inn i en tabell, linje for linje.
+    /// </summary>
+    /// <param name="filplassering">Plasseringen til filen + navn og filtype. eks: test.txt</param>
+    /// <param name="filefound">Metoden vil indikere om den finner filen eller ikke</param>
+    /// <param name="fjernOverskriftLinjer">Hvor mange linjer du vil skippe på starten av filen</param>
+    /// <returns>Returnerer en tabell med alle linjene med tekst fra filen. tabell[0] inneholder feilmelding om filefound=false</returns>
+    public static string[] LesFraFil(string filplassering, ref bool filefound, int fjernOverskriftLinjer=0)
+    {
+        try
+        {
+            string[] textlinjerFraFil = System.IO.File.ReadAllLines(filplassering);
+            if (fjernOverskriftLinjer != 0)
+                Array.Copy(textlinjerFraFil, fjernOverskriftLinjer, 
+                          (textlinjerFraFil=new string[textlinjerFraFil.Length- fjernOverskriftLinjer]),
+                          0, textlinjerFraFil.Length);
+            filefound = true;
+            return textlinjerFraFil;
+        }
+        catch (System.IO.FileNotFoundException e)
+        {
+            filefound = false;
+            return new string[]{"feilmelding"};
+        }
+    }
+
+    /// <summary>
+    /// Åpner en fil og leser den inn i en tabell, linje for linje.
+    /// </summary>
+    /// <param name="filplassering">Plasseringen til filen + navn og filtype. eks: test.txt</param>
+    /// <param name="fjernOverskriftLinjer">Hvor mange linjer du vil skippe på starten av filen</param>
+    /// <returns>Returnerer en tabell med alle linjene med tekst, minus fjernOverskiftLinjer antall linjer fra starten av filen.</returns>
+    public static string[] LesFraFil(string filplassering, int fjernOverskriftLinjer = 0)
+    {
+        string[] textlinjerFraFil = System.IO.File.ReadAllLines(filplassering);
+
+        if (fjernOverskriftLinjer != 0)
+        {
+            Array.Copy(textlinjerFraFil, fjernOverskriftLinjer, (textlinjerFraFil = new string[textlinjerFraFil.Length - fjernOverskriftLinjer]), 0, textlinjerFraFil.Length);
+        }
+        return textlinjerFraFil;
+    }
+
+}
---
+++ oppg3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oppg3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MAX_TEMP = 100;
+            const int MIN_TEMP = 80;
+            List<string> linjerFraFil = new List<string>(Commons.LesFraFil("oppg3-data.txt", 2));
+            List<string> tider = new List<string>();
+            List<double> verdier = new List<double>();
+
+            foreach(string s in linjerFraFil)
+            {
+                string[] linje = s.Split(' ');
+                string tid = linje[0];
+                double verdi1 = Convert.ToDouble(linje[1]);
+                double verdi2 = Convert.ToDouble(linje[2]);
+                double gjennomsnitt = (verdi1 + verdi2) / 2;
+                tider.Add(tid);
+                verdier.Add(gjennomsnitt);
+
+                if(verdi1>MAX_TEMP || verdi1 < MIN_TEMP)
+                {
+                    Console.WriteLine("Alarm fra sensor A, verdi: " + verdi1 +" klokken "+ tid);
+                }
+                if (verdi2 > MAX_TEMP || verdi2 < MIN_TEMP)
+                {
+                    Console.WriteLine("Alarm fra sensor B, verdi: " + verdi2 + " klokken " + tid);
+                }
+            }
+
+            Console.WriteLine("Linjner lest: " + linjerFraFil.Count);
+            Console.WriteLine("Første tid: " + tider.First<string>());
+            Console.WriteLine("Siste tid: " + tider.Last<string>());
+
+            string[] tilFil = new string[linjerFraFil.Count];
+            for(int i = 0; i < linjerFraFil.Count; i++)
+            {
+                tilFil[i] = String.Format("{0} {1:f2}", tider[i], verdier[i]);
+            }
+
+            Commons.SkrivTilFil("oppg3-rapport.txt", tilFil);
+
+            Console.ReadKey();
+        }
+    }
+}
